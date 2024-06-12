cc6833f8599d313e2e9d0e778aa460a0f486487e
admin <yooan_u@naver.com>
2024-06-12 23:42:09 +0900

    0612

--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 var mapContainer= document.getElementById('map'), // 지도를표시할div 
     mapOption= {
-        center: newkakao.maps.LatLng(37, 131), // 지도의중심좌표
-        level: 3 // 지도의확대레벨
+        center: new kakao.maps.LatLng(37, 131), // 지도의중심좌표
+        level: 3 // 지도의 확대 레벨
         };
-var map= newkakao.maps.Map(mapContainer, mapOption); // 지도를생성
-// 일반지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도 타입 컨트롤을 생성
-var mapTypeControl= newkakao.maps.MapTypeControl();
+var map= new kakao.maps.Map(mapContainer, mapOption); // 지도를생성
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도 타입 컨트롤을 생성
+var mapTypeControl= new kakao.maps.MapTypeControl();
 // 지도에 컨트롤을 추가해야 지도위에표시
 // kakao.maps.ControlPosition은컨트롤이표시위치TOPRIGHT는오른쪽위를의미
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 // 지도 확대축소를 제어할 수 있는 줌 컨트롤을 생성
-var zoomControl= newkakao.maps.ZoomControl();
+var zoomControl= new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
